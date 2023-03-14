06701a149886ab65ea5f28f1075afe36e0710e96
Mysteryeditor <sri027.cvb@gmail.com>
2023-03-14 20:27:38 +0530

    task3commit

--- scriptfile.js
+++ scriptfile.js
@@ -124,4 +124,62 @@ function comma(){
 function dt(){
     alert(Date());
 }
-// task2 question2
+// task 3
+class Person{
+constructor(name,age,gen,addr)
+{
+this.uname = name;
+this.age = age;
+this.gen = gen;
+this.addr = addr;
+}
+display()
+{
+var x = "Name:"+this.uname+"<br> Age:"+this.age+"<br Gender:"+this.gen+"<br> Address:"+this.addr;
+document.getElementById("show").innerHTML=x;
+}
+}
+function task3_1(){
+let Obj = new Person("Srikanth",20,"Male","Coimbatore");
+Obj.display();
+}
+
+
+class Person2{
+constructor(name,age,gen,addr)
+{
+this.uname = name;
+this.age = age;
+this.gen = gen;
+this.addr = addr;
+}
+display(){}
+}
+
+class Employee extends Person2{
+constructor(empname,empage,empgen,empaddr,empcode,empdept,empdesg,empsal){
+super(empname,empage,empgen,empaddr);
+this.empname=empname;
+this.empage = empage;
+this.empgen = empgen;
+this.empaddr = empaddr;
+this.empcode = empcode;
+this.empdept = empdept;
+this.empdesg = empdesg;
+this.empsal = empsal;
+}
+
+display()
+{
+
+let x = "Name:"+this.empname+"<br> Age:"+this.empage+"<br> Gender:"+this.empgen+"<br> Address:"+this.empaddr+"<br> Designation:"+this.empdesg+"<br> Code:"+this.empcode+"<br> Salary:"+this.empsal;
+document.getElementById("show").innerHTML=x;
+}
+}
+
+function task3_2()
+{
+var obj2 = new Employee("Elon Musk",50,"male","America",73263,"IT","CEO",352000);
+obj2.display();
+}
+
